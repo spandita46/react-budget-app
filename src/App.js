94cee4d0543ecb0133632060fd45978c4557c36b
Sandeep Pandita <sandeeppandita46@gmail.com>
2022-02-22 16:06:10 -0500

    base skeleton app working

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1> Hello, World! </h1>
+      <h1> Set Up From Scratch In A Long Run </h1>
+    </div>
+  );
+};
+
+export default App;
